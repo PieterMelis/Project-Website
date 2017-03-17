70cfd2c42eb705726350fddfff2a4b7a171bc487
tymo.dekock <tymo.dekock>
2017-03-17 15:13:36 +0100

    added migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,15 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default("User"); 
+            /**
+            *Current user roles are: Admin / Editor / Approver / User / Student
+            * Admin is super user -> can do everything everyone else can. Admin also gives roles to other users.
+            * Editor is the main source for articles
+            * Approver checks the articles and either approves or declines them depending on quality.
+            * User is the main visitor of the website. Hopefully students that want to come to antwerp. 
+            * Student users are students from Antwerp (this is here for future chat functionality)
+            */
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,5 +40,9 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('comments');
+
+
     }
 }
---
+++ database/migrations/2017_03_17_134146_create_articles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->string('excerpt'); //Excerpt is small part of body to show on summary pages
+            $table->string('body');
+            $table->string('type'); //Types are: Testimonial, Video, Text
+
+            //Links still need to be optimized. For now an article can have 2 images (or 1 img 1 video) links
+            $table->string('link1');
+            $table->string('link2');
+
+            $table->timestamps();
+
+            //link article to creating editor            
+            $table->foreign('user_id')
+                  ->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
