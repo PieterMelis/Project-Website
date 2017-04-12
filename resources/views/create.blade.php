6cf92dc8521b4e0d32df0455a2ab5bf2f02fe629
pieter melis <pieter.melis@hotmail.com>
2017-04-12 16:12:28 +0200

    style.css

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,79 +2,140 @@
 
 
 @section('content')
-	@if (Auth::guest())
-        <h2><a href="/Project-Web-Git/public/home">Log in first!</h2>
-    @else
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+	<div class="container">
+		@if (Auth::guest())
+					<div class="row">
+							<div class="col-md-8 col-md-offset-2">
 
-            <!-- Display Validation Errors -->
-            @if ($errors->any())
-            <div class="alert alert-danger">
-                <strong>Whoops! Something went wrong!</strong>
-                <br><br>
-                <ul>
-                    
-                        @foreach($errors->all() as $error)
-                            <li>{{$error}}</li>
-                        @endforeach
-                    
-                </ul>
-            </div>
-           @endif
-            
+											<div class="panel-body">
+													<form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+															{{ csrf_field() }}
+
+																<h3>Log in first</h3>
+																<hr>
+															<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+																	<label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+																	<div class="col-md-6">
+																			<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+																			@if ($errors->has('email'))
+																					<span class="help-block">
+																							<strong>{{ $errors->first('email') }}</strong>
+																					</span>
+																			@endif
+																	</div>
+															</div>
+
+															<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+																	<label for="password" class="col-md-4 control-label">Password</label>
+
+																	<div class="col-md-6">
+																			<input id="password" type="password" class="form-control" name="password" required>
+
+																			@if ($errors->has('password'))
+																					<span class="help-block">
+																							<strong>{{ $errors->first('password') }}</strong>
+																					</span>
+																			@endif
+																	</div>
+															</div>
+
+															<div class="form-group">
+																	<div class="col-md-6 col-md-offset-4">
+																			<div class="checkbox">
+																					<label>
+																							<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+																					</label>
+																			</div>
+																	</div>
+															</div>
+
+															<div class="form-group">
+																	<div class="col-md-8 col-md-offset-4">
+																			<button type="submit" class="btn btn-primary">
+																					Login
+																			</button>
+
+																			<a class="btn btn-link" href="{{ route('password.request') }}">
+																					Forgot Your Password?
+																			</a>
+																	</div>
+															</div>
+													</form>
+											</div>
+							</div>
+					</div>
+	    @else
+	    <div class="row">
+	        <div class="col-md-10 col-md-offset-1">
+
+	            <!-- Display Validation Errors -->
+	            @if ($errors->any())
+	            <div class="alert alert-danger">
+	                <strong>Whoops! Something went wrong!</strong>
+	                <br><br>
+	                <ul>
+
+	                        @foreach($errors->all() as $error)
+	                            <li>{{$error}}</li>
+	                        @endforeach
+
+	                </ul>
+	            </div>
+	           @endif
 
 
-             <div class="breadcrumb">
-                
-                <a href="/project-web-git/public">← back to overview</a>
 
-            </div>
-            
-            <div class="panel panel-default">
-                <div class="panel-heading">Add article</div>
+	             <div class="breadcrumb">
+	                <a href="/home">← back to overview</a>
+	            </div>
 
-                <div class="panel-content">               
-                        {!! Form::open() !!}
 
-                            <div class="form-group">
-                                {!! Form::label('title', 'Title (max. 255 characters')  !!}
-                                {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                            </div>
-                            <div class="form-group">
-                                {!! Form::label('excerpt', 'Excerpt') !!}
-                                {!! Form::text('excerpt', null, ['class' => 'form-control']) !!}
-                            </div>
-                            <div class="form-group">
-                                {!! Form::label('body', 'Body') !!}
-                                {!! Form::textArea('body', null, ['class' => 'form-control']) !!}
-                            </div>  
-                            <div class="form-group">
-                                {!! Form::label('type', 'Type') !!}
-                                {!! Form::text('type', null, ['class' => 'form-control']) !!}
-                            </div>     
-                            <div class="form-group">
-                                {!! Form::label('link1', 'upload 1') !!}
-                                {!! Form::text('link1', null, ['class' => 'form-control']) !!}
-                            </div>  
-                            <div class="form-group">
-                                {!! Form::label('link2', 'upload 2') !!}
-                                {!! Form::text('link2', null, ['class' => 'form-control']) !!}
-                            </div>  
-                            <div class="form-group"> 
-                                                                                        
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-plus"></i> Add Article
-                                </button>
+									<h3>Article toevoegen</h3>
+									<hr>
 
-                            </div>
-                            
-                            {!! Form::close() !!}            
-                </div>
-            </div>
+	                <div class="panel-content">
+	                        {!! Form::open() !!}
 
-        </div>
-    </div>
-    @endif
+	                            <div class="form-group">
+	                                {!! Form::label('title', 'Title (max. 255 characters')  !!}
+	                                {!! Form::text('title', null, ['class' => 'form-control']) !!}
+	                            </div>
+	                            <div class="form-group">
+	                                {!! Form::label('excerpt', 'Excerpt') !!}
+	                                {!! Form::text('excerpt', null, ['class' => 'form-control']) !!}
+	                            </div>
+	                            <div class="form-group">
+	                                {!! Form::label('body', 'Body') !!}
+	                                {!! Form::textArea('body', null, ['class' => 'form-control']) !!}
+	                            </div>
+	                            <div class="form-group">
+	                                {!! Form::label('type', 'Type') !!}
+	                                {!! Form::text('type', null, ['class' => 'form-control']) !!}
+	                            </div>
+	                            <div class="form-group">
+	                                {!! Form::label('link1', 'upload 1') !!}
+	                                {!! Form::text('link1', null, ['class' => 'form-control']) !!}
+	                            </div>
+	                            <div class="form-group">
+	                                {!! Form::label('link2', 'upload 2') !!}
+	                                {!! Form::text('link2', null, ['class' => 'form-control']) !!}
+	                            </div>
+	                            <div class="form-group">
+
+	                                <button type="submit" class="btn btn-default">
+	                                    <i class="fa fa-plus"></i> Add Article
+	                                </button>
+
+	                            </div>
+
+	                            {!! Form::close() !!}
+	                </div>
+
+	    </div>
+	    @endif
+
+	</div>
 
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="/css/style.css">
-
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
       <div class="full">
