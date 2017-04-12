cd80c6da29cf2cf3515c0f685d4bb378c12dd9d5
pieter melis <pieter.melis@hotmail.com>
2017-04-12 16:25:25 +0200

    edit stylesheet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Rajdhani" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     </head>
     <body>
       <div class="full">
