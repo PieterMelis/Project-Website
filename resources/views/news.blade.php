35128b4fa1a8dc5ad0a69514b712950eadc88720
pieter melis <pieter.melis@hotmail.com>
2017-05-18 15:01:29 +0200

    news fix

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -8,17 +8,80 @@
 
 
 
-          <div class="row x">
+
+
+
+   <div class="row x">
+
+
+            <div class="col-md-6 art ">
+
+                <a href="#">
+                    <img class="img-responsive " src="{{ url('img/kot.jpg') }}" alt="">
+                </a>
+                <div class="text">
+                  <h3>
+                     test
+                 </h3>
+                   <p>test</p>
+                   <i class="fa fa-plus" aria-hidden="true"></i><a href="#">  Lees meer</a>
+                </div>
+              </div>
+              <div class="col-md-6 art ">
+                <a href="#">
+                    <img class="img-responsive " src="{{ url('img/Antwerp.jpg') }}" alt="">
+                </a>
+                <div class="text">
+                  <h3>
+                     test
+                 </h3>
+                   <p>test</p>
+                   <i class="fa fa-plus" aria-hidden="true"></i><a href="#">  Lees meer</a>
+                </div>
+              </div>
+
+
+            <div class="col-md-6 art ">
+                <a href="#">
+                    <img class="img-responsive " src="{{ url('img/Antwerp2.jpg') }}" alt="">
+                </a>
+                <div class="text">
+                  <h3>
+                     test
+                 </h3>
+                   <p>test</p>
+                   <i class="fa fa-plus" aria-hidden="true"></i><a href="#">  Lees meer</a>
+                </div>
+              </div>
+                <div class="col-md-6 art ">
+                <a href="#">
+                    <img class="img-responsive " src="{{ url('img/Antwerp.jpg') }}" alt="">
+                </a>
+                <div class="text">
+                  <h3>
+                     test
+                 </h3>
+                   <p>test</p>
+                   <i class="fa fa-plus" aria-hidden="true"></i><a href="#">  Lees meer</a>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
+          <div class="row ">
               <div class="col-lg-12">
                   <h1 class="page-header">Nieuwsartikels
                   </h1>
               </div>
           </div>
          <!-- Projects Row -->
-         <div class="row">
+         <div class="row x">
 
           @foreach ($textArticles as $article)
-           
+
              <div class="col-md-6 art">
                  <a href="article/{{ $article->id }}">
                      <img class="img-responsive " src="../storage/app/{{$article->link1}}" alt="">
@@ -32,7 +95,7 @@
                  </div>
                </div>
 
-           
+
          @endforeach
 
          </div>
