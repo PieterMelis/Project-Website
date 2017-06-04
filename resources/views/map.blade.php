afbddff843e4333b1881fed64dafcb87a50081e2
pieter melis <pieter.melis@hotmail.com>
2017-06-04 19:38:29 +0200

    pull stuf

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -7,4 +7,10 @@
   <iframe class="createMap" src="https://www.scribblemaps.com/create/#id=htkri2ix9v"  height="1000" width="100%"></iframe>
   @endif
 
+  <!--
+  <iframe class="createMap" src="https://uebermaps.com/maps/4215-antwerp-student&startpage=cover"  height="1000" width="100%"></iframe>
+
+  <iframe class="createMap" src="https://uebermaps.com/embed?map_id=4215-antwerp-student&map_token=7606-L7Vha6Dz5BfBSs8QTjyu&startpage=map" src="https://google.be"   height="1000" width="100%"></iframe>
+
+  -->
 @endsection
