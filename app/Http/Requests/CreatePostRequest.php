8964837860c176e065671335175714764fcb2060
tymo.dekock <tymo.dekock>
2017-06-08 20:19:39 +0200

    Changed error messages

--- app/Http/Requests/CreatePostRequest.php
+++ app/Http/Requests/CreatePostRequest.php
@@ -32,4 +32,19 @@ class CreatePostRequest extends FormRequest
 
         ];
     }
+    public function messages(){
+         return [
+        'title.required' => 'De titel mag niet leeg zijn!',
+        'title.max'      => 'De titel mag niet langer zijn dan 255 tekens!',
+        'excerpt.required' => 'De samenvatting mag niet leeg zijn!',
+        'excerpt.max' => 'De samenvatting mag niet langer zijn dan 190 tekens!',
+        'link1.required' => 'Het eerste bestand mag niet leeg zijn!',
+        'link1.max' => 'Het eerste bestand mag niet groter zijn dan 100MB!'
+        'link1.mimes' => 'Het eerste bestand is niet het juiste formaat!'
+        'link2.max' => 'Het tweede bestand mag niet groter zijn dan 100MB!'
+        'link2.mimes' => 'Het tweede bestand is niet het juiste formaat!'
+        'body.required'  => 'De inhoud mag niet leeg zijn!',
+    ];
+
+    }
 }
